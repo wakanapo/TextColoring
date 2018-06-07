19ff23d5be5032de6485af06915c35fdcc6589c4
wakanapo <wakana.tn16@gmail.com>
2018-06-07 16:24:51 +0900

    Fixed typo.

--- sample.cc
+++ sample.cc
@@ -5,7 +5,7 @@ int main() {
   std::cout << "The color variation is "
             << coloring::coloringText("Red, ", coloring::Color::RED)
             << coloring::coloringText("Green, ", coloring::Color::GREEN)
-            << coloring::coloringText("blue, ", coloring::Color::BLUE)
+            << coloring::coloringText("Blue, ", coloring::Color::BLUE)
             << coloring::coloringText("Yellow, ", coloring::Color::YELLOW)
             << coloring::coloringText("Magenta, ", coloring::Color::MAGENTA)
             << coloring::coloringText("Cyan, ", coloring::Color::CYAN)
